d4e5794eb19d326e4972764cc4aaf2e77f1d0ba8
Ofer Goldstein <tulioz@gmail.com>
2011-04-10 02:09:09 -0700

    I hope this doesn't break, crossing my fingers!

---
+++ iOS/UCD Events/UCD Events/InfoViewController.h
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.h
+//  UCD Events
+//
+//  Created by Ofer Goldstein on 4/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface InfoViewController : TTTableViewController {
+    
+}
+
+@end
---
+++ iOS/UCD Events/UCD Events/InfoViewController.m
@@ -0,0 +1,14 @@
+//
+//  InfoViewController.m
+//  UCD Events
+//
+//  Created by Ofer Goldstein on 4/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+
+@implementation InfoViewController
+
+@end
