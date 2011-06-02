f23d05e1026a2b482c9fae61edc6c91c2901783b
terrrydactyl <tcho708@gmail.com>
2011-06-02 00:23:29 -0700

    Initialized new variables from Settings.h

--- iOS/UCD Events/Settings.m
+++ iOS/UCD Events/Settings.m
@@ -14,14 +14,27 @@ const NSString *apiDataFormat       = @"json";
 const NSString *typesString         = @"types";
 const NSString *locationsString     = @"locations";
 const NSString *offersString        = @"offers";
+const NSString *eventsString        = @"events";
 
-const NSString *UCDPoliceDepartmentPhone    = @"5307475400";
-const NSString *UCDPoliceDepartmentAddr     = @"2600 5th Street, Davis, CA 95618";
-const NSString *UCDFireDepartmentPhone      = @"4084312535";
-const NSString *UCDFireDepartmentAddr       = @"530 5th Street, Davis, CA 95616";
+const NSString *UCDPDEmergencyPhone 		= @"5307521230";
+const NSString *UCDPDNonEmergencyPhone	    = @"5307521727";
+const NSString *UCDFireDepartmentPhone      = @"5307521236";
+const NSString *UCDFireDepartmentAddr       = @"625 Kleiber Hall Dr, Davis, CA 95616";
+const NSString *UCDFireLat					= @"38.5409453";
+const NSString *UCDFireLong					= @"-121.7557253";
 const NSString *SutterDHSPhone              = @"5307566440";
 const NSString *SutterDHSAddr               = @"2000 Sutter Place, Davis, CA 95616";
-const NSString *eventsString                = @"events";
+const NSString *SutterDHSLat				= @"38.562764";
+const NSString *SutterDHSLong				= @"-121.7720747";
+const NSString *DavisPDPhone				= @"5307475400";
+const NSString *DavisPDAddr					= @"2600 5th Street, Davis, CA 95618";
+const NSString *DavisPDLat					= @"38.551843";
+const NSString *DavisPDLong					= @"-121.7187756";
+const NSString *DavisFirePhone				= @"5307575684";
+const NSString *DavisFireAddr				= @"530 5th Street, Davis, CA 95616";
+const NSString *DavisFireLat				= @"38.54684";
+const NSString *DavisFireLong				= @"-121.742462";
+
 NSString *eventsTitleString                 = @"Featured Events";
 
 NSString *UCDButtonURL        = @"";
