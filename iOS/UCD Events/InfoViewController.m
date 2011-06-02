c97484dc2aa077e5ab6a3bb185bd41ddeee03e3c
terrrydactyl <tcho708@gmail.com>
2011-06-02 00:36:06 -0700

    Fixed a phone number bug.

--- iOS/UCD Events/InfoViewController.m
+++ iOS/UCD Events/InfoViewController.m
@@ -65,7 +65,7 @@
 	// City of Davis Fire Dept
 	NSString *DavisFDurl = [self openGoogleMapsWithDestinationLatitude:DavisFireLat longitude:DavisFireLong];
     NSArray *DavisFD = [NSArray arrayWithObjects:
-                      [TTTableCaptionItem itemWithText:[self formatPhoneString:DavisPDPhone] caption:@"phone" URL:[NSString stringWithFormat:@"tel:%@", DavisFirePhone]],
+                      [TTTableCaptionItem itemWithText:[self formatPhoneString:DavisFirePhone] caption:@"phone" URL:[NSString stringWithFormat:@"tel:%@", DavisFirePhone]],
                       [TTTableCaptionItem itemWithText:DavisFireAddr caption:@"address" URL:DavisFDurl],
                       nil
                       ];
